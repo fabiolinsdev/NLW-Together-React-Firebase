ed147fb8c628a6354eea2cd6b503d0043e4c751c
fabiolinsdev <binhobsb_fabio29@outlook.com>
2021-06-27 14:08:35 -0300

    Primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { Button } from './components/Button'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { room } from './pages/Room';
+import { adminRoom } from './pages/adminRoom';
+
+//import { AuthContextProvider } from "./contexts/AuthContext"
 
 function App() {
   return (
-   <div>
-     <Button />
-     <Button />
-     <Button />
-     <Button />
-     <Button />
-   </div>
+    <BrowserRouter>
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+      <Route path="/rooms/:id" component={room} />
+
+      <Route path="/admin/rooms/:id" component={adminRoom} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,18 @@
-export function Button() {
-    const counter = 0;
+import { ButtonHTMLAttributes } from "react";
+
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutliner?: boolean
+};
+
+export function Button({ isOutlined = false, ...props }: ButtonProps) {
     return (
-        <button>0</button>
+        <button 
+        className={`button ${isOutlined ? 'outlined' : ''}`}
+        {...props} 
+        />
+         
     )
 }
 
